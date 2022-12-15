d2f42010ce11e3bc462c6220b158f3e8129016fd
AbsoluteCrime <52426993+AbsoluteCrime@users.noreply.github.com>
2022-12-15 13:14:29 -0500

    AOS

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,4 +1,5 @@
 import "../styles/style.css";
+import { menu } from "./menu";
 import javascriptLogo from "/favicon.svg";
 import { setupCounter } from "../counter.js";
 
@@ -10,7 +11,7 @@ document.querySelector("#app").innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite! </h1>
+    <h1>Heya Vite! </h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
@@ -21,3 +22,4 @@ document.querySelector("#app").innerHTML = `
 `;
 
 setupCounter(document.querySelector("#counter"));
+console.log(menu);
